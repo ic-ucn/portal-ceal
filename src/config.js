869a2a0c9ca986bc34c5f70b8859ed0f373a64d8
schernandez5 <kevin.cortes@alumnos.ucn.cl>
2026-06-22 20:12:54 -0400

    Corrige client id OAuth nuevo

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 window.PORTAL_GOOGLE_DOMAIN = window.PORTAL_GOOGLE_DOMAIN || 'alumnos.ucn.cl';
-window.PORTAL_GOOGLE_CLIENT_ID = window.PORTAL_GOOGLE_CLIENT_ID || '26073251977-1amsfcbgnrhmb1slujj59ia5pmv0dleq.apps.googleusercontent.com';
+window.PORTAL_GOOGLE_CLIENT_ID = window.PORTAL_GOOGLE_CLIENT_ID || '26073251977-1amsfcbqnrhmb1sluij59ia5pmv0dleq.apps.googleusercontent.com';
 window.PORTAL_API_BASE = window.PORTAL_API_BASE || 'https://portal-ceic-api.onrender.com/api';
 window.PORTAL_AI_ENDPOINT = window.PORTAL_AI_ENDPOINT || '';
